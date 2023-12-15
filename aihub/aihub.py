cccf9bfd928ef097bb59687cb8eb65a78926535b
Csaba Kecskemeti <kecso@Csabas-MacBook-Pro.local>
2023-12-15 07:16:38 -0800

    initial GUI

--- aihub/aihub.py
+++ aihub/aihub.py
@@ -10,7 +10,7 @@ from logging import handlers
 import time
 
 
-version = 'beta'
+version = '1.0 beta'
 COMBINATIONS = [
     {keyboard.Key.shift, keyboard.KeyCode(char='a')},
     {keyboard.Key.shift, keyboard.KeyCode(char='A')},
@@ -48,6 +48,24 @@ class ClickListener:
         return self.coordinates
 
 
+def system_printer(text, is_gui=False):
+    if is_gui:
+        print(f'\n{text}')
+    else:
+        print(f'\n{bcolors.HEADER}{text}{bcolors.ENDC}')
+
+def user_printer(text, is_gui=False):
+    if is_gui:
+        print(f'\n{text}')
+    else:
+        print(f'\n{bcolors.WARNING}{text}{bcolors.ENDC}')
+
+def bot_printer(text, is_gui=False):
+    if is_gui:
+        print(f'\n{text}')
+    else:
+        print(f'\n{bcolors.OKGREEN}{text}{bcolors.ENDC}')
+
 def capture_screen():
     # Create an instance of ClickListener
     click_listener = ClickListener()
@@ -80,27 +98,28 @@ def send_request(user_input:str, host:str, port:int):
     return response.json()['choices'][0]['message']['content']
 
 
-def help_me(host:str, port:int):
+def help_me(host:str, port:int, is_gui=False):
     image = capture_screen()
     if image != None:
         text = pytesseract.image_to_string(image)
         # PROMPT_DEFAULT = "Summarize this: \n"
         PROMPT_DEFAULT = "Help me with this: \n"
         prompt = PROMPT_DEFAULT + text
-        print(f'\n{bcolors.WARNING}USER:{bcolors.ENDC}')
-        print(f'{bcolors.WARNING}{prompt}{bcolors.ENDC}')
+        user_printer('USER:', is_gui)
+        user_printer(prompt, is_gui)
         return send_request(prompt, host, port)
     pass
 
 
 def main():
-
-    print(f'{bcolors.HEADER}I am © aiHub | version: {version} | © devquasar.com{bcolors.ENDC}')
-    parser = argparse.ArgumentParser(description='A simple argparse script')
+    parser = argparse.ArgumentParser(description='aiHub arg parse')
     parser.add_argument('-api', '--llm_api_host', type=str, help='LLM API host', default='http://localhost')
     parser.add_argument('-p', '--port', type=int, help='LLM API port number', default=1234)
     parser.add_argument('-l', '--log_file', type=str, help='Log file for aiHub', default='aihub.log')
+    parser.add_argument('-gui', '--gui_printer', action='store_true', help='Using aiHub with aiHubManager GUI')
     args = parser.parse_args()
+
+    system_printer(f'I am © aiHub | version: {version} | © devquasar.com', args.gui_printer)
 
     def log_setup():
         log_handler = logging.handlers.WatchedFileHandler(args.log_file)
@@ -117,11 +136,11 @@ def main():
             logging.info(f'key combo pressed: {current}')
             # if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
             if any(x in current for x in {keyboard.KeyCode(char='A')}):
-                print(f'{bcolors.HEADER}Make a screenshot: define the screen area by click 2 corners of a rectangle{bcolors.ENDC}')
+                system_printer(f'Make a screenshot: define the screen area by click 2 corners of a rectangle', args.gui_printer)
 
-                bot_response = help_me(args.llm_api_host, args.port)
-                print(f'\n{bcolors.OKGREEN}BOT:{bcolors.ENDC}')
-                print(f'{bcolors.OKGREEN}{bot_response}{bcolors.ENDC}')
+                bot_response = help_me(args.llm_api_host, args.port, args.gui_printer)
+                bot_printer('BOT:', args.gui_printer)
+                bot_printer(bot_response, args.gui_printer)
             # if key == keyboard.Key.shift and any(x in current for x in {keyboard.KeyCode(char='X')}):
             if any(x in current for x in {keyboard.KeyCode(char='X')}):
                 logging.info('Shift + X pressed. Stopping the listener.')
---
+++ aihub/aihub_gui.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import scrolledtext
+import subprocess
+
+class MyApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("aiHub manager")
+
+        # Text area to display stdout
+        self.text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD)
+        self.text_area.pack(padx=10, pady=10, expand=True, fill="both")
+
+        # Button to start the background app
+        self.start_button = tk.Button(root, text="Start aiHub lurking...", command=self.start_background_app)
+        self.start_button.pack(pady=10)
+
+        # Button to stop the background app
+        self.stop_button = tk.Button(root, text="Stop aiHub", command=self.stop_background_app,
+                                     state=tk.DISABLED)
+        self.stop_button.pack(pady=5)
+
+        # Store the process and file descriptor
+        self.process = None
+        self.fd = None
+
+    def start_background_app(self):
+        # Disable the button while the app is running
+        self.start_button.config(state=tk.DISABLED)
+
+        # Start the background app
+        self.process = subprocess.Popen(
+            ["python", "-u", "aihub.py", "-gui"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            bufsize=1,
+            universal_newlines=True,
+        )
+
+        # Get the file descriptor for stdout
+        self.fd = self.process.stdout.fileno()
+
+        # Set up a fileevent to monitor the stdout for new data
+        self.root.createfilehandler(self.fd, tk.READABLE, self.read_stdout)
+
+    def stop_background_app(self):
+        # Terminate the background app
+        if self.process:
+            self.process.terminate()
+
+        # Enable the start button
+        self.start_button.config(state=tk.NORMAL)
+
+        # Disable the stop button
+        self.stop_button.config(state=tk.DISABLED)
+A
+    def read_stdout(self, *args):
+        # Read and display the stdout in real-time
+        line = self.process.stdout.readline()
+        if line:
+            self.update_text_area(line)
+        else:
+            # The process has finished, enable the button
+            self.start_button.config(state=tk.NORMAL)
+
+    def update_text_area(self, text):
+        # Update the text area in a thread-safe manner
+        self.text_area.insert(tk.END, text)
+        self.text_area.yview(tk.END)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MyApp(root)
+    root.mainloop()
